d14a7e6d7e0366cbb5cb760d20b1cbd2af4b94ed
Harsh Jadhav <harshjadhav084@gmail.com>
2024-04-23 17:32:34 +0530

    Add files via upload

---
+++ Concatination/Concatination.java
@@ -0,0 +1,17 @@
+class Concatenation 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(10+20);
+		System.out.println(12.5 + 5);
+		System.out.println('a' + 10);
+		System.out.println("10"+20);
+		System.out.println("5" + 5);
+		System.out.println("sheela" + 420);
+		System.out.println(10 + 20 + "hello"+20);
+		System.out.println(10+"hii"+(20+40));
+		System.out.println("laila" + true);
+		System.out.println("laila"+"true");
+
+	}
+}
---
+++ Concatination/Concatination1.java
@@ -0,0 +1,10 @@
+class  Concatination1
+{
+	public static void main(String[] args) 
+	{
+		String name = "mala";
+		int age = 25;
+		System.out.println("My name is:" + name);
+		System.out.println("my age is " + age);
+	}
+}
